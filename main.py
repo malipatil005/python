65f5f1b09cf0aef09c5717abf86e4dc8561ef856
malipatil005 <chandu.patil005@gmail.com>
2024-04-10 22:58:21 +0530

    added the constructor and created object.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 
 class myclass:
     """this is simple class"""
-    def __init__(self):
-        print("this is simple class")
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def show_details(self):
+        print("name=",self.name)
+        print("age=",self.age)
+
+
 
 
 
 if __name__=="__main__":
-    c=myclass()
+    c=myclass("deepika",27)
+    c.show_details()
